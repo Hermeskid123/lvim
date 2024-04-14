de98596485ecc3f5939188c67e3060ad2c4c2116
Hermeskid123 <preston.mann@mavs.uta.edu>
2024-04-14 19:54:41 +0000

    changed stuff

--- config.lua
+++ config.lua
@@ -1,60 +1,40 @@
-vim.opt.mouse = "nv"
-
--- general
-lvim.log.level = "warn"
-lvim.format_on_save = true
-lvim.colorscheme = "sonokai"
-vim.cmd("let g:sonokai_style = 'andromeda'")
-vim.cmd("let g:sonokai_enable_italic_comment = 1")
-vim.opt.shiftwidth = 4
-
-lvim.builtin.terminal.size = 90
-lvim.builtin.terminal.direction = "vertical"
-lvim.keys.normal_mode["<C-b>"] = "<esc><cmd>ToggleTermSendCurrentLine<CR>"
-lvim.keys.visual_mode["<C-b>"] = ":'<,'>ToggleTermSendVisualLines<CR>"
-lvim.keys.term_mode["<C-l>"] = "<C-l>"
--- to disable icons and use a minimalist setup, uncomment the following
--- lvim.use_icons = false
-
--- keymappings [view all the defaults by pressing <leader>Lk]
-lvim.leader = "space"
--- add your own keymapping
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
-lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
-
-lvim.keys.normal_mode["<F2>"] = ":set spell!<CR>" -- toggle spell check
-lvim.keys.normal_mode["<F4>"] = ":set spelllang=en<CR>"
-lvim.keys.normal_mode["<F3>"] = ":set spelllang=pt_br<CR>"
-
-
-
-
-
-
--- TODO: User Config for predefined plugins
--- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
-
--- if you don't want all the parsers change this to a table of the ones you want
-lvim.builtin.treesitter.ensure_installed = {
-    "bash",
-    "c",
-    "javascript",
-    "json",
-    "lua",
-    "python",
-    "typescript",
-    "tsx",
-    "css",
-    "rust",
-    "java",
-    "yaml",
+-- install plugins
+lvim.plugins = {
+    "ChristianChiarulli/swenv.nvim",
+    "stevearc/dressing.nvim",
+    "mfussenegger/nvim-dap-python",
+    "nvim-neotest/neotest",
+    "nvim-neotest/neotest-python",
 }
 
-lvim.builtin.treesitter.ignore_install = { "haskell" }
-lvim.builtin.treesitter.highlight.enabled = true
+-- automatically install python syntax highlighting
+lvim.builtin.treesitter.ensure_installed = {
+    "python",
+}
+
+-- setup formatting
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup { { name = "black" }, }
+lvim.format_on_save.enabled = true
+lvim.format_on_save.pattern = { "*.py" }
+
+-- setup linting
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup { { command = "flake8", filetypes = { "python" } } }
+
+-- setup debug adapter
+lvim.builtin.dap.active = true
+local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
+pcall(function()
+    require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
+end)
+
+
+lvim.colorscheme = "onedarker"
+vim.g.tokyonight_style = "night"
+vim.opt.mouse = "nv"
+--allows copy past
+--
+lvim.log.level = "warn"
+lvim.format_on_save = true
+vim.opt.shiftwidth = 4
